52ae3f00ac1127e975b36ac47f64bd7f8e960094
Christian Perez <perezchristian1012@gmail.com>
2019-10-29 23:58:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,6 @@ class Student
     @grade = grade
     @id = id
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
   def self.create_table
     sql = <<-SQL
